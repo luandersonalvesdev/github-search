9c292583b49c213c3825d6c57099b3f79ee83096
Luanderson Alves <luaoderson@gmail.com>
2023-05-31 00:42:29 -0300

    feat: created service for make a user fetch or repo fetch

---
+++ app/service/customFetch.js
@@ -0,0 +1,20 @@
+const CORRECT_STATUS = 200;
+const URL_GITHUB = 'https://api.github.com/users/';
+
+export const fetchUser = async (username) => {
+  const response = await fetch(`${URL_GITHUB}${username}`);
+  if (response.status === CORRECT_STATUS) {
+    const data = await response.json();
+    return data;
+  }
+  return ({ ok: false });
+};
+
+export const fetchRepo = async (username) => {
+  const response = await fetch(`${URL_GITHUB}${username}/repos`);
+  if (response.status === CORRECT_STATUS) {
+    const data = await response.json();
+    return data;
+  }
+  return ({ ok: false });
+};
